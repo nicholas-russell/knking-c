00b87e71a9a67278862998cef3833407f974f7af
nicholas-russell <nrussell97@outlook.com>
2022-05-15 21:15:52 +1000

    Completed chapter 4

--- chapter4/c4_5.c
+++ chapter4/c4_5.c
@@ -12,7 +12,7 @@ int main(int argc, char const *argv[])
 
     first_sum = d + m2 + m4 + p1 + p3 + p5;
     second_sum = m1 + m3 + m5 + p2 + p4;
-    total = 3 * first_sum +second_sum;
+    total = 3 * first_sum + second_sum;
 
     printf("Check digit: %d\n", 9 - ((total - 1) % 10));
     
